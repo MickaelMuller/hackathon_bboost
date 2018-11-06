e7cde1a6c9066154134cad71167f91ea3fdfeaf0
AnaMilten <anaismiltenberger@gmail.com>
2018-11-06 15:39:22 +0100

    footer v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Footer from './Component/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <div className="content">
         epidemium
+        </div>
+        <Footer />
       </div>
     );
   }

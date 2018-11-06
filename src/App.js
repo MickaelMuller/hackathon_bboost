f8d8ce1c064d3f4b06f12db05c898686c6ea14e0
MickaelMuller <Muller-Mickael@live.fr>
2018-11-06 16:44:34 +0100

    cardpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import CardPage from './CardPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardPage />
       </div>
     );
   }

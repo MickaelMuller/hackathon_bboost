3cf7820b0487373b8404b8489f49afe8a60f0dd9
Alexandre <buissonalexandre16@gmail.com>
2018-11-06 14:39:23 +0100

    NavBar v1

---
+++ src/components/FormUpload.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FormUpload extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default FormUpload;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './navbar.css';
+import { Navbar, NavbarBrand, NavItem } from 'reactstrap';
+import { NavbarNav } from 'mdbreact';
+import { NavLink } from 'react-router-dom';
+
+
+
+class NavBar extends Component {
+    render() {
+        return (
+            <Navbar className="navcolor" dark expand="xs" scrolling fixed='top'>
+                <NavbarBrand href="/">
+                    <img className='logo' src='/medias/LogoEpidemiumV2.png' alt='Logo' />
+                </NavbarBrand>
+                <NavbarNav left>
+                    <NavItem className="links">
+                        Link1
+                    </NavItem>
+                    <NavItem className="links">
+                        Link2
+                    </NavItem>
+                    <NavItem className="links">
+                        Link3
+                    </NavItem>
+                </NavbarNav>
+                <NavbarNav right>
+                    <NavItem>
+                        <form className="form-inline md-form mt-0">
+                             <input placeholder="Search..."/>
+                        </form>
+                    </NavItem>
+                </NavbarNav>
+            </Navbar>
+        );
+    }
+}
+export default NavBar;

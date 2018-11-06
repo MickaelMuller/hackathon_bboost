c6c376a48ccc9ae5622fb9aef8056ebeb306b4a3
MickaelMuller <Muller-Mickael@live.fr>
2018-11-06 17:23:05 +0100

    modif scrol lateral

--- src/components/home/FormUpload.jsx
+++ src/components/home/FormUpload.jsx
@@ -1,43 +1,46 @@
-import React, { Component } from 'react';
-import { Button, Input, Container, Row, Col } from 'reactstrap';
-import './FormUpload.css'
+import React, { Component } from "react";
+import { Button, Input, Container, Row, Col } from "reactstrap";
+import "./FormUpload.css";
 
 class FormUpload extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (
-            <Container>
-                <Row>
-                    <Col>
-                        <Input type="file" className="btnparcourir">Parcourir...</Input>
-                        <Input type="text" placeholder="Titre"></Input>
-                        <br/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Input placeholder="Abcisse X"></Input>
-                    </Col>
-                    <Col>                  
-                        <Input placeholder="Ordonée Y"></Input>
-                    </Col>
-                </Row>
-                <br/>
-                <Row>
-                    <Col>
-                        <Input type="textarea" placeholder="Tags"></Input>
-                        <Button className="btnsubmit">Envoyer</Button>
-                    </Col>
-                </Row>
-                    
-                
-            </Container> 
-            
-         );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col lg="4">
+            <Input type="file" className="btnparcourir">
+              Parcourir...
+            </Input>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Input type="text" placeholder="Titre" />
+          </Col>
+        </Row>
+        <br/>
+        <Row>
+          <Col>
+            <Input placeholder="Abcisse X" />
+          </Col>
+          <Col>
+            <Input placeholder="Ordonée Y" />
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col>
+            <Input type="textarea" placeholder="Tags" />
+            <Button className="btnsubmit">Envoyer</Button>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
- 
+
 export default FormUpload;

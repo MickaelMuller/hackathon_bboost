6befcf40154f3bd1685921d629ff98c9e7362ced
AnaMilten <anaismiltenberger@gmail.com>
2018-11-07 10:03:41 +0100

    correction des bugs footer, espaces entre sections, ajout de séparateurs

--- src/components/home/FormUpload.jsx
+++ src/components/home/FormUpload.jsx
@@ -9,47 +9,49 @@ class FormUpload extends Component {
     this.state = {};
     this.handleChange = this.handleChange.bind(this);
   }
-  
+
   handleChange() {
 
   }
   render() {
     return (
-      <Container className="margin-top">
-        <h2 className="form-text">Créez votre graphique</h2>
-        <Row>
-          <Col lg="4">
-            <Input
-              type="file"
-              onChange={this.handleChange()}
-              className="btnparcourir"
-            >
-              Parcourir...
+      <div className="form-top">
+        <Container >
+          <h2 className="form-text">Créez votre graphique</h2>
+          <Row>
+            <Col lg="4">
+              <Input
+                type="file"
+                onChange={this.handleChange()}
+                className="btnparcourir"
+              >
+                Parcourir...
             </Input>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Input type="text" placeholder="Titre" />
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Col>
-            <Input placeholder="Abcisse X" />
-          </Col>
-          <Col>
-            <Input placeholder="Ordonée Y" />
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Col>
-            <Input type="textarea" placeholder="Tags" className="form-spacing"/>
-            <Button className="btnsubmit">Envoyer</Button>
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Input type="text" placeholder="Titre" />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col>
+              <Input placeholder="Abcisse X" />
+            </Col>
+            <Col>
+              <Input placeholder="Ordonée Y" />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col>
+              <Input type="textarea" placeholder="Tags" className="form-spacing" />
+              <Button className="btnsubmit">Envoyer</Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
